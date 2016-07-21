5bf503a6f922245062f7a109bc564eafec61a1b8
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-07-21 16:31:08 +0200

    Moved files to other package.

--- net_diffu/network_diffusion/src/main/java/net/digital_alexandria/diffusion/kernels/Kernel.java
+++
@@ -1,9 +0,0 @@
-package net.digital_alexandria.diffusion.kernels;
-
-/**
- * @author Simon Dirmeier {@literal simon.dirmeier@gmx.de}
- */
-public interface Kernel
-{
-    public double map(double[] x1, double[] x2);
-}
--- net_diffu/network_diffusion/src/main/java/net/digital_alexandria/structs/enums/DataType.java
+++
@@ -1,9 +0,0 @@
-package net.digital_alexandria.structs.enums;
-
-/**
- * @author Simon Dirmeier {@literal simon.dirmeier@gmx.de}
- */
-public enum DataType
-{
-    DataTable, FLAT
-}
