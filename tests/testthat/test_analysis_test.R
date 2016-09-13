434c3ffd8b85248435d017481d48cc2b6212c19f
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-09-13 23:25:28 +0200

    created analysis/preprocess test files

---
+++ tests/testthat/test_analysis_test.R
@@ -0,0 +1,9 @@
+context("analysis")
+
+data(plate)
+
+test_that("bscore", {
+  expect_equal(1,1)
+})
+
+
---
+++ tests/testthat/test_preprocess_viability.R
@@ -0,0 +1,9 @@
+context("preprocess")
+
+data(plate)
+
+test_that("bscore", {
+  expect_equal(1,1)
+})
+
+
