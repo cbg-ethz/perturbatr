9953dec46713afd2432a3946b4febce64d480a08
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-03-09 21:03:38 +0100

    [feature] started integration to S4

--- R/data.R
+++ R/data.R
@@ -24,8 +24,9 @@
 #' @author Simon Dirmeier | Computational Biology Group, ETH ZURICH | \email{simon.dirmeier@@bsse.ethz.ch}
 #' @name plate-data
 #'
+#' @noRd
 #' @docType data
 #' @keywords datasets data
 #' @usage data(plate)
 #' @format A data.table with 18 columns, each describing a necessary feature of a \code{svd.raw} data-set
-"plate"
+NA
