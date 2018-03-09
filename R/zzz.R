559a3a092f0bd3f4836e8c2647b6036df6c779c0
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-03-09 22:51:29 +0100

    Adds globalVariables call to remove NOTE

---
+++ R/zzz.R
@@ -0,0 +1,50 @@
+# perturbatr: analysis of high-throughput gene perturbation screens
+#
+# Copyright (C) 2018 Simon Dirmeier
+#
+# This file is part of perturbatr
+#
+# perturbatr is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# perturbatr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with perturbatr. If not, see <http://www.gnu.org/licenses/>.
+
+#' @importFrom utils globalVariables
+utils::globalVariables(
+  c(
+    ".",
+    "boot",
+    "Boot",
+    "Control",
+    "Count",
+    "Condition",
+    "cnt",
+    "DiffusionEffect",
+    "Effect",
+    "Genes",
+    "GeneConditionEffect",
+    "GenePathID",
+    "GeneSymbol",
+    "grp",
+    "lo",
+    "Lower",
+    "Mean",
+    "MeanEffect",
+    "method",
+    "Perturbation",
+    "Pval",
+    "Qval",
+    "Replicate",
+    "Readout",
+    "Replicates",
+    "Type",
+    "Upper"),
+  package = "perturbatr")
