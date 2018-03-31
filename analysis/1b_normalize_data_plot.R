1d99c002b5852b186afa8613b3484e16cf52429c
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-03-31 23:59:55 +0200

    Updates 1b and 2

--- analysis/1b_normalize_data_plot.R
+++ analysis/1b_normalize_data_plot.R
@@ -1,33 +1,45 @@
 #!/usr/bin/env Rscript
 
-library(data.table)
-library(dtplyr)
+# perturbatr: analysis of high-throughput gene perturbation screens
+#
+# Copyright (C) 2018 Simon Dirmeier
+#
+# This file is part of perturbatr
+#
+# perturbatr is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# perturbatr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with perturbatr. If not, see <http://www.gnu.org/licenses/>.
+
+
+library(tibble)
 library(dplyr)
 library(tidyr)
-library(lme4)
-library(optparse)
-library(perturbatr)
 library(ggplot2)
-library(hashmap)
-library(ggthemr)
-library(hrbrthemes)
-library(viridis)
 library(cowplot)
+library(perturbatr)
 
-ggthemr("fresh", "scientific")
 
 plot.data <- function(x)
 {
   numb.frame <-
-    dplyr::group_by(x@.data, Virus, Screen) %>%
+    dplyr::group_by(x, Condition, Screen) %>%
     dplyr::summarize(Replicates = length(unique(Replicate)),
                      Genes      = length(unique(GeneSymbol))) %>%
     tidyr::gather(Type, Count, Replicates, Genes)
   numb.frame$Count <- as.integer(numb.frame$Count)
 
   pl <-
-    ggplot2::ggplot(numb.frame, ggplot2::aes(x=Virus, y = Count)) +
-    ggplot2::geom_bar(ggplot2::aes(fill=Virus), stat="identity") +
+    ggplot2::ggplot(numb.frame, ggplot2::aes(x=Condition, y = Count)) +
+    ggplot2::geom_bar(ggplot2::aes(fill=Condition), stat="identity") +
     ggplot2::scale_y_continuous(breaks=scales::pretty_breaks(5)) +
     ggplot2::facet_grid(Type ~ Screen, scales='free_y') +
     ggplot2::geom_text(ggplot2::aes(label = Count, y = Count), size = floor(20/3), vjust=0) +
@@ -45,24 +57,24 @@ plot.data <- function(x)
                    panel.spacing.y = ggplot2::unit(2, "lines"))
 }
 
+
 plot.quality <- function(x)
 {
-  qual <- x@.data
-  qual <- dplyr::group_by(qual, Virus, Screen, Library,
+  qual <- x
+  qual <- dplyr::group_by(qual, Condition, Screen, Library,
                           ScreenType, ReadoutType,
                           Replicate, Plate) %>%
-    dplyr::mutate(Plate = .GRP) %>%
-    ungroup
+    {dplyr::mutate(ungroup(.), grp = group_indices(.)) }%>%
+    ungroup()
   df <- qual %>%
-    dplyr::select(Virus, Screen, Readout, Plate) %>%
-    dplyr::mutate(Plate=as.factor(Plate)) %>%
+    dplyr::select(Condition, Screen, Readout, grp) %>%
+    dplyr::mutate(grp=as.factor(grp)) %>%
     dplyr::filter(!is.na(Readout), !is.infinite(Readout))
 
 
-  ggplot2::ggplot(df, ggplot2::aes(Plate, Readout)) +
+  ggplot2::ggplot(df, ggplot2::aes(grp, Readout)) +
     ggplot2::geom_boxplot() +
     ggplot2::theme_bw() +
-
     ggplot2::theme(aspect.ratio=.75,
       text = ggplot2::element_text(size = 20),
                    axis.text.x=element_blank(),
@@ -74,14 +86,15 @@ plot.quality <- function(x)
 
 }
 
+
 run <- function()
 {
 
-  path <- "./"
-  out.dir <- "./plots"
+  data.dir <- "./data"
+  out.dir  <- "./plots"
 
-  raw.data.file  <- paste(path, "data/rnai_screen_raw.rds", sep="/")
-  norm.data.file <- paste(path, "data/rnai_screen_normalized.rds", sep="/")
+  raw.data.file  <- paste(data.dir, "rnai_screen_raw.rds", sep="/")
+  norm.data.file <- paste(data.dir, "rnai_screen_normalized_2.rds", sep="/")
 
   raw.dat  <- readRDS(raw.data.file)
   norm.dat <- readRDS(norm.data.file)
@@ -94,18 +107,17 @@ run <- function()
     width = 10,
     height = 10)
 
-  pl.raw  <- perturbatr::filter(raw.dat, Virus=="HCV", Screen=="Kinome") %>%
-    plot.quality
-  pl.norm  <- perturbatr::filter(norm.dat, Virus=="HCV", Screen=="Kinome") %>%
+  pl.raw  <- dplyr::filter(raw.dat, Condition == "HCV", Screen=="Kinome") %>%
+    plot.quality()
+  pl.norm  <- dplyr::filter(norm.dat, Condition == "HCV", Screen=="Kinome") %>%
     plot.quality + ylab("")
-  pl.gr <- plot_grid(pl.raw, pl.norm, align="h", labels=c("(a)", "(b)"))
+  pl.gr <- cowplot::plot_grid(pl.raw, pl.norm, align="h", labels=c("(a)", "(b)"))
 
   ggsave(
     filename = paste0(out.dir, "/", "comparison_plate_readouts", ".eps"),
     plot = pl.gr,
     width = 12,
-    height = 6  )
-
+    height = 6)
 }
 
 run()
--- analysis/2_model_selection.R
+++ analysis/2_model_selection.R
@@ -1,14 +1,14 @@
 #!/usr/bin/env Rscript
 
 library(dplyr)
-library(dtplyr)
-library(data.table)
+library(tibble)
 library(perturbatr)
 library(lme4)
 library(utils)
 library(tidyr)
 library(formula.tools)
 options(warn=-1)
+
 
 do.lmm <- function(model.data, formulae)
 {
@@ -25,118 +25,191 @@ do.lmm <- function(model.data, formulae)
   lmms
 }
 
+
 find.best <- function(lmm.fits, method=c("BIC", "logLik"))
 {
   method <- match.arg(method)
-  tab <- sapply(lmm.fits, function(e) {  c(Model=as.character(formula(e)),  AIC=AIC(e), BIC=BIC(e), logLik=logLik(e)) }) %>% t %>% as.data.table
-  setDT(tab)[, logLik := as.double(logLik)]
-  setDT(tab)[, BIC := as.double(BIC)]
+  tab <- sapply(
+    lmm.fits,
+    function(e)
+    {
+      c(Model=as.character(formula(e)),  AIC=AIC(e), BIC=BIC(e), logLik=logLik(e))
+    }
+  ) %>%
+    t() %>%
+    as.tibble() %>%
+    dplyr::mutate(logLig = as.double(logLik), BIC = as.double(BIC))
+
   l <- list()
   if (method == "BIC") {
     l <- list(Best=tab$Model[which.min(tab$BIC)], Models=tab)
   } else {
     l <- list(Best=tab$Model[which.max(tab$logLik)], Models=tab)
   }
+
   l
 }
 
-mixed.effects.model.selection.stacking <- function(rnai.screen, model.data, starting.models)
+
+mixed.effects.model.selection.stacking <- function(rnai.screen,
+                                                   model.data,
+                                                   starting.models)
 {
   single.model.strings <- c("Cell", "ScreenType", "ReadoutType", "Design")
-  model.formulas <- c(starting.models, paste0(starting.models, " + (1 | ", single.model.strings, ")"))
-  lmm.fits <- do.lmm(model.data, model.formulas)
+  model.formulas  <- c(starting.models,
+                       paste0(starting.models,
+                              " + (1 | ", single.model.strings, ")"))
+  lmm.fits        <- do.lmm(model.data, model.formulas)
   best.lmm.single <- find.best(lmm.fits)
 
-  dual.model.strings <- expand.grid(single.model.strings, single.model.strings) %>%
-    apply(1, sort) %>% t %>% as.data.table %>%
-    unique %>%
+  dual.model.strings <- expand.grid(single.model.strings,
+                                    single.model.strings) %>%
+    apply(1, sort) %>%
+    t() %>%
+    as.tibble() %>%
+    unique() %>%
     dplyr::filter(V1 != V2)
+
   model.formulas <- c(best.lmm.single$Best,
-                      paste0(starting.models, " + (1 | ", dual.model.strings$V1, ") + (1 | ", dual.model.strings$V2, ")"))
+                      paste0(starting.models,
+                             " + (1 | ",
+                             dual.model.strings$V1,
+                             ") + (1 | ",
+                             dual.model.strings$V2, ")"))
+
   lmm.fits <- do.lmm(model.data, model.formulas)
   best.lmm.double <- find.best(lmm.fits)
 
-
-  triple.model.strings <- expand.grid(single.model.strings, single.model.strings, single.model.strings)  %>%
-    apply(1, sort) %>% t %>% as.data.table %>%
-    unique %>%
+  triple.model.strings <- expand.grid(single.model.strings,
+                                      single.model.strings,
+                                      single.model.strings)  %>%
+    apply(1, sort) %>%
+    t() %>%
+    as.tibble() %>%
+    unique() %>%
     dplyr::filter(V1 != V2 & V1 != V3 & V2 != V3)
-  model.formulas <- c(best.lmm.double$Best, paste0(starting.models, " + (1 | ", triple.model.strings$V1, ") + (1 | ", triple.model.strings$V2, ") + (1 | " ,   triple.model.strings$V3, ")"))
+
+  model.formulas <- c(best.lmm.double$Best,
+                      paste0(starting.models,
+                             " + (1 | ",
+                             triple.model.strings$V1,
+                             ") + (1 | ",
+                             triple.model.strings$V2,
+                             ") + (1 | ",
+                             triple.model.strings$V3, ")"))
   lmm.fits <- do.lmm(model.data, model.formulas)
   best.lmm.triple <- find.best(lmm.fits)
 
-  model.formulas <- c(best.lmm.triple$Best, "Readout ~ Virus + (1 | GeneSymbol) + (1 | Cell) +  (1 | Design) +  (1 | ScreenType) + (1 | ReadoutType)")
+  model.formulas <- c(
+    best.lmm.triple$Best,
+    "Readout ~ Condition + (1 | GeneSymbol) + (1 | Cell) +  (1 | Design) +  (1 | ScreenType) + (1 | ReadoutType)")
   lmm.fits <- do.lmm(model.data, model.formulas)
   best.lmm <- find.best(lmm.fits)
 
   best.lmm
 }
 
-mixed.effects.model.selection.aggregating <- function(  rnai.screen, model.data, starting.models)
+
+mixed.effects.model.selection.aggregating <- function(rnai.screen,
+                                                      model.data,
+                                                      starting.models)
 {
   single.model.strings <- c("Cell", "ScreenType", "ReadoutType", "Design")
   model.formulas <- c(starting.models,
-                      paste0(starting.models,  " + (1 | ", single.model.strings, ")"))
-  lmm.fits <- do.lmm(model.data, model.formulas)
+                      paste0(starting.models,
+                             " + (1 | ",
+                             single.model.strings,
+                             ")"))
+  lmm.fits        <- do.lmm(model.data, model.formulas)
   best.lmm.single <- find.best(lmm.fits)
 
   dual.model.strings <- expand.grid(single.model.strings, single.model.strings)  %>%
-    apply(1, sort) %>% t %>%
-    as.data.table %>% unique %>%
+    apply(1, sort) %>%
+    t() %>%
+    as.tibble() %>%
+    unique() %>%
     dplyr::filter(V1 != V2)
+
   model.formulas <- c(best.lmm.single$Best,
-                      paste0(starting.models, " + (1 | ", dual.model.strings$V1, ") + (1 | ", dual.model.strings$V2, ")"))
-  lmm.fits <- do.lmm(model.data, model.formulas)
+                      paste0(starting.models,
+                             " + (1 | ",
+                             dual.model.strings$V1,
+                             ") + (1 | ",
+                             dual.model.strings$V2,
+                             ")"))
+
+  lmm.fits        <- do.lmm(model.data, model.formulas)
   best.lmm.double <- find.best(lmm.fits)
 
-  triple.model.strings <- expand.grid(single.model.strings, single.model.strings, single.model.strings)  %>%
-    apply(1, sort) %>% t %>% as.data.table %>% unique %>%
+  triple.model.strings <- expand.grid(single.model.strings,
+                                      single.model.strings,
+                                      single.model.strings)  %>%
+    apply(1, sort) %>%
+    t() %>%
+    as.tibble() %>%
+    unique() %>%
     dplyr::filter(V1 != V2 & V1 != V3 & V2 != V3)
+
   model.formulas <- c(best.lmm.double$Best,
-                      paste0(starting.models, " + (1 | ", triple.model.strings$V1, ") + (1 | ", triple.model.strings$V2, ") + (1 | " ,   triple.model.strings$V3, ")"))
-  lmm.fits <- do.lmm(model.data, model.formulas)
+                      paste0(starting.models,
+                             " + (1 | ",
+                             triple.model.strings$V1,
+                             ") + (1 | ",
+                             triple.model.strings$V2,
+                             ") + (1 | " ,
+                             triple.model.strings$V3, ")"))
+
+  lmm.fits        <- do.lmm(model.data, model.formulas)
   best.lmm.triple <- find.best(lmm.fits)
 
-  model.formulas <- c(best.lmm.triple$Best
-                      , paste0(starting.models, " + (1 | Cell) +  (1 | Design) +  (1 | ScreenType) + (1 | ReadoutType)"))
+  model.formulas <- c(
+    best.lmm.triple$Best,
+    paste0(starting.models, " + (1 | Cell) +  (1 | Design) +  (1 | ScreenType) + (1 | ReadoutType)"))
+
   lmm.fits <- do.lmm(model.data, model.formulas)
   best.lmm <- find.best(lmm.fits)
+
   best.lmm
 }
 
-fixed.effects.model.selection.stacking <- function(rnai.screen, model.data, starting.models)
+
+fixed.effects.model.selection.stacking <- function(rnai.screen,
+                                                   model.data,
+                                                   starting.models)
 {
   single.model.strings <- c("Cell", "ReadoutType", "Design")
-  model.formulas <- c(starting.models)
+  model.formulas       <- c(starting.models)
   for (i in seq(length(single.model.strings)))
   {
-    co <- utils::combn(single.model.strings, i) %>% t
+    co <- utils::combn(single.model.strings, i) %>% t()
     for (j in 1:nrow(co))
     {
       c <- co[j, ]
-      mo <- paste0("Readout ~ Virus + ", paste(c , collapse=" + "), " + (1 | GeneSymbol)")
-      mo2 <- paste0("Readout ~ Virus + ", paste(c , collapse=" + "), " + (1 | GeneSymbol) + (1 | Virus:GeneSymbol)")
+      mo  <- paste0("Readout ~ Condition + ", paste(c , collapse=" + "), " + (1 | GeneSymbol)")
+      mo2 <- paste0("Readout ~ Condition + ", paste(c , collapse=" + "), " + (1 | GeneSymbol) + (1 | Condition:GeneSymbol)")
       model.formulas <- c(model.formulas, mo, mo2)
     }
   }
+
   lmm.fits <- do.lmm(model.data, model.formulas)
   best.lmm.single <- find.best(lmm.fits)
 
   single.model.strings <- c("Cell", "ReadoutType", "Design")
-  model.formulas <- c(best.lmm.single$Best)
+  model.formulas       <- c(best.lmm.single$Best)
   for (i in seq(length(single.model.strings)))
   {
     co <- utils::combn(single.model.strings, i) %>% t
     for (j in 1:nrow(co))
     {
-      c <- co[j, ]
-      mo <- paste0("Readout ~ Virus + ", paste(c , collapse=" + "), " + (1 | GeneSymbol)")
-      mo2 <- paste0("Readout ~ Virus + ", paste(c , collapse=" + "), " + (1 | GeneSymbol) + (1 | Virus:GeneSymbol)")
-      mo3 <- paste0("Readout ~ Virus + ", paste(c , collapse=" + "), " + (1 | GeneSymbol) + (1 | Virus:GeneSymbol) + (1 | ScreenType)")
-      mo4 <- paste0("Readout ~ Virus + ", paste(c , collapse=" + "), " + (1 | GeneSymbol) + (1 | Virus:GeneSymbol) + (1 | ScreenType) + (1 | ScreenType:GeneSymbol)")
+      c   <- co[j, ]
+      mo  <- paste0("Readout ~ Condition + ", paste(c , collapse=" + "), " + (1 | GeneSymbol)")
+      mo2 <- paste0("Readout ~ Condition + ", paste(c , collapse=" + "), " + (1 | GeneSymbol) + (1 | Condition:GeneSymbol)")
+      mo3 <- paste0("Readout ~ Condition + ", paste(c , collapse=" + "), " + (1 | GeneSymbol) + (1 | Condition:GeneSymbol) + (1 | ScreenType)")
+      mo4 <- paste0("Readout ~ Condition + ", paste(c , collapse=" + "), " + (1 | GeneSymbol) + (1 | Condition:GeneSymbol) + (1 | ScreenType) + (1 | ScreenType:GeneSymbol)")
       model.formulas <- c(model.formulas, mo, mo2)
     }
   }
+
   lmm.fits <- do.lmm(model.data, model.formulas)
   best.lmm <- find.best(lmm.fits)
 
@@ -146,30 +219,31 @@ fixed.effects.model.selection.stacking <- function(rnai.screen, model.data, star
 run <- function()
 {
   cat(paste("Loading data\n"))
-  rna.file <- "data/rnai_screen_normalized.rds"
+  rna.file    <- "data/rnai_screen_normalized_2.rds"
   rnai.screen <- readRDS(rna.file)
   cat(paste("Setting model data\n"))
-  model.data <- perturbatr::set.lmm.model.data(rnai.screen, drop=T)@.data
+  model.data <- methods::as(rnai.screen, "PerturbationData")
+  model.data <- perturbatr:::setModelData(model.data, drop=T)
 
   cat(paste("Doing model selection on non-nested models\n"))
-  starting.models       <- c("Readout ~ Virus + (1 | GeneSymbol)", "Readout ~ Virus + (1 | GeneSymbol) + (1 | Virus:GeneSymbol)")
+  starting.models       <- c("Readout ~ Condition + (1 | GeneSymbol)", "Readout ~ Condition + (1 | GeneSymbol) + (1 | Condition:GeneSymbol)")
   best.stacked.model    <- mixed.effects.model.selection.stacking(rnai.screen, model.data, starting.models)
   best.aggregated.model <- mixed.effects.model.selection.aggregating(rnai.screen, model.data, best.stacked.model$Best)
   best.stacked.model.2  <- mixed.effects.model.selection.stacking(rnai.screen, model.data, best.aggregated.model$Best)
   cat(paste(best.stacked.model.2, "\n"))
 
   # the last call gives as result:
-  # "Readout ~ Virus + (1 | GeneSymbol) + (1 | Virus:GeneSymbol) + (1 | ScreenType)"
+  # "Readout ~ Condition + (1 | GeneSymbol) + (1 | Condition:GeneSymbol) + (1 | ScreenType)"
   # from here subgrouping random effects can be tested
   cat(paste("Doing model selection on nested-models\n"))
-  formu <- c("Readout ~ Virus + (1 | GeneSymbol) + (1 | Virus:GeneSymbol) + (1 | ScreenType)",
-             "Readout ~ Virus + (1 | GeneSymbol) + (1 | Virus:GeneSymbol) + (1 | ScreenType) + (1 | Virus:ScreenType)")
+  formu <- c("Readout ~ Condition + (1 | GeneSymbol) + (1 | Condition:GeneSymbol) + (1 | ScreenType)",
+             "Readout ~ Condition + (1 | GeneSymbol) + (1 | Condition:GeneSymbol) + (1 | ScreenType) + (1 | Condition:ScreenType)")
   fin <- do.lmm(model.data, formu)
-  # result : take  "Readout ~ Virus + (1 | GeneSymbol) + (1 | Virus:GeneSymbol) + (1 | ScreenType) + (1 | Virus:ScreenType)"
+  # result : take  "Readout ~ Condition + (1 | GeneSymbol) + (1 | Condition:GeneSymbol) + (1 | ScreenType) + (1 | Condition:ScreenType)"
   lmerTest::anova(fin[[1]], fin[[2]])
-  starting.models <- c("Readout ~ Virus + (1 | GeneSymbol) + (1 | Virus:GeneSymbol) + (1 | ScreenType) + (1 | Virus:ScreenType)",
-                       "Readout ~ Virus + (1 | GeneSymbol) + (1 | Virus:GeneSymbol) + (1 | ScreenType)",
-                       "Readout ~ Virus + (1 | GeneSymbol) + (1 | Virus:GeneSymbol)")
+  starting.models <- c("Readout ~ Condition + (1 | GeneSymbol) + (1 | Condition:GeneSymbol) + (1 | ScreenType) + (1 | Condition:ScreenType)",
+                       "Readout ~ Condition + (1 | GeneSymbol) + (1 | Condition:GeneSymbol) + (1 | ScreenType)",
+                       "Readout ~ Condition + (1 | GeneSymbol) + (1 | Condition:GeneSymbol)")
   cat(paste("Doing model selection on fixed effets models\n"))
   best.saturated.model <- fixed.effects.model.selection.stacking(rnai.screen, model.data, starting.models)
 
