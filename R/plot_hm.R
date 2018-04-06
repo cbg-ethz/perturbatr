cc940cdfcffe50d44425d3e6438bcf5f1c39ca1c
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-04-06 15:24:18 +0200

    New vignette

--- R/plot_hm.R
+++ R/plot_hm.R
@@ -39,21 +39,14 @@
 plot.HMAnalysedPerturbationData <- function(x, size=10, main="", ...)
 {
   pl <- try({
-    .plot.perturbation.hm.analysed(
-      geneHits(x), main=main, size=size)
+    .plot.perturbation.hm.analysed(geneHits(x), main=main, size=size)
   })
   pl2 <- try({
-    .plot.perturbation.hm.analysed(nestedGeneHits(x), main="", size=size) +
-      ggplot2::facet_wrap( ~ Condition,
-        ncol=ceiling(length(unique(nestedGeneHits(x)$Condition))/2))
-  })
-  pl3 <- try({
     .plot.effect.matrices.perturbation.analysed.hm(x, size)
   })
 
   return(list(gene.effect.barplot        = pl,
-              nested.gene.effect.barplot = pl2,
-              nested.gene.effect.matrix  = pl3))
+              nested.gene.effect.matrix  = pl2))
 }
 
 
@@ -63,18 +56,17 @@ plot.HMAnalysedPerturbationData <- function(x, size=10, main="", ...)
 #' @importFrom dplyr group_by filter row_number
 #' @importFrom methods hasArg
 #' @importFrom rlang .data
-.plot.perturbation.hm.analysed  <- function(x, main, size, ...)
+.plot.perturbation.hm.analysed  <- function(x, main, size)
 {
-  pars <- list(...)
   x <- dplyr::filter(x, .data$Control == 0)
   x <- x[base::order(-abs(x$Effect)), ]
-  if ("Condition" %in% colnames(x))
-  {
+  if ("Condition" %in% colnames(x)) {
     x <- dplyr::group_by(x, .data$Condition)
   }
   x <- dplyr::filter(x, row_number() <= 25)
-  x$GeneSymbol <- base::factor(x$GeneSymbol, levels=rev(unique(x$GeneSymbol)))
-  pl <- .plot.bars(x, size, main, ...)
+  x$GeneSymbol <- base::factor(x$GeneSymbol, levels=(unique(x$GeneSymbol)))
+  pl <- .plot.bars(x, size, main)
+
   pl
 }
 
@@ -110,11 +102,14 @@ plot.HMAnalysedPerturbationData <- function(x, size=10, main="", ...)
     ggplot2::theme_minimal() +
     ggplot2::theme(text=ggplot2::element_text(size = size,
                                               family = "Helvetica"),
+                   legend.position = "bottom",
                    aspect.ratio = 2,
-                   axis.text.x  = ggplot2::element_text(angle=45,
+                   axis.text.x  = ggplot2::element_text(angle=0,
                                                         hjust = 1,
-                                                        size=size),
-                   axis.text.y  = ggplot2::element_text(size=size),
+                                                        size=size,
+                                                        family = "Helvetica"),
+                   axis.text.y  = ggplot2::element_text(size=size,
+                                                        family = "Helvetica"),
                    axis.title   = ggplot2::element_blank(),
                    axis.ticks   = ggplot2::element_blank())
   pl
@@ -123,16 +118,24 @@ plot.HMAnalysedPerturbationData <- function(x, size=10, main="", ...)
 
 #' @noRd
 #' @importFrom rlang .data
-.plot.bars <- function(x, size, main, ...)
+.plot.bars <- function(x, size, main)
 {
+  pl <-
     ggplot2::ggplot(x) +
     ggplot2::geom_bar(
       ggplot2::aes(x$GeneSymbol, abs(x$Effect), fill=factor(sign(x$Effect))),
-      stat="identity") +
-    ggplot2::scale_fill_manual("Trend",
-      values = c(colors()$red, "grey", colors()$blue),
-      limits = c("1",  "0", "-1"),
-      labels=c("Positive", "None", "Negative")) +
+      stat="identity")
+  pl <- if (any(sign(x$Effect) == 0)) {
+    pl + ggplot2::scale_fill_manual(
+      "Trend", values = c(colors()$red, "grey", colors()$blue),
+      limits = c("1",  "0", "-1"), labels=c("Positive", "None", "Negative"))
+  } else {
+    pl + ggplot2::scale_fill_manual(
+      "Trend", values = c(colors()$red,  colors()$blue),
+      limits = c("1", "-1"), labels=c("Positive", "Negative"))
+  }
+  pl <-
+    pl +
     ggplot2::scale_x_discrete(labels = rev(sort(x$GeneSymbol)),
                               limits = rev(sort(x$GeneSymbol))) +
     ggplot2::theme_minimal() +
@@ -140,7 +143,8 @@ plot.HMAnalysedPerturbationData <- function(x, size=10, main="", ...)
                                                        family = "Helvetica"),
                    axis.title.x = ggplot2::element_blank(),
                    axis.title.y = ggplot2::element_blank(),
-                   axis.ticks=ggplot2::element_blank(),
+                   axis.ticks   = ggplot2::element_blank(),
+                   panel.grid.major = ggplot2::element_blank(),
                    text = ggplot2::element_text(size = size,
                                                 family = "Helvetica"),
                    axis.text.x = ggplot2::element_text(
@@ -148,4 +152,6 @@ plot.HMAnalysedPerturbationData <- function(x, size=10, main="", ...)
                                               family = "Helvetica")) +
     ggplot2::coord_flip() +
     ggplot2::ggtitle(main)
+
+  pl
 }
